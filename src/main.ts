d413713be1330d40033a3643ea172ce0ae310755
egou <helloegou@qq.com>
2026-01-14 18:00:27 +0800

    check format husky

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,16 @@ createApp(App).mount('#app')
 const a = 1
 console.log(a)
 
-function test()
-
-
-
-{
-
-
-
-
+function test() {
   console.log('"Heollo World",test')
-
-
-  ;;;;
-
-
-  
 }
+
+
+console.log("我就是用来测试代码提交前是否被格式化再提交!")
+
+
+
+
+;
 
 test()
