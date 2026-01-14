0ee367aac346c95872ef1c1a172f88c9eaf2d0f7
egou <helloegou@qq.com>
2026-01-14 18:06:30 +0800

    format

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,9 @@ const a = 1
 console.log(a)
 
 function test() {
-  console.log('"Heollo World",test');
-  
+  console.log('"Heollo World",test')
 }
 
 console.log('我就是用来测试代码提交前是否被格式化再提交!')
-;
 
 test()
-;
